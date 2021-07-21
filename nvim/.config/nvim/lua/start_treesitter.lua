86e72cb24432256c6c25322f1b9e3e822f7834df
Caio Dadauto <caiodadauto@gmail.com>
2021-07-21 13:39:05 -0300

    ignore haskell for treesitter

--- nvim/.config/nvim/lua/start_treesitter.lua
+++ nvim/.config/nvim/lua/start_treesitter.lua
@@ -2,6 +2,7 @@ local tree = require('nvim-treesitter.configs')
 
 tree.setup {
     ensure_installed = "all",
+    ignore_install = { "haskell" },
     highlight = {
         enable = true,
         disable = { "haskell"},

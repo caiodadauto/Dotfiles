afa29d82083c45c34590864fa44d636eda3515bf
Caio Dadauto <caiodadauto@gmail.com>
2021-07-21 11:52:07 -0300

    .

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -14,7 +14,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cd $HOMW/.cargo/bin
+cd $HOME/.cargo/bin
 ./cargo install $(awk '{print $1}' ../programs/cargo.list)
 
 # Starship

e8986d4c3bd6d20ca14a3eb34ecf66f65a868401
Caio Dadauto <caiodadauto@gmail.com>
2021-07-21 19:02:29 -0300

    libbbb

--- scripts/set-python.sh
+++ scripts/set-python.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip install $(awk '{print $1}' ../programs/pip.list)
+pip pyarrow==4.0.* "dask[complete]"
 
 jupyter nbextension install --py --sys-prefix jupyter_ascending
 jupyter nbextension     enable jupyter_ascending --sys-prefix --py

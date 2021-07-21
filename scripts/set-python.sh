fad7cd38f725f00a4629a8566855d35e810f2572
Caio Dadauto <caiodadauto@gmail.com>
2021-07-21 17:54:00 -0300

    jupyter

--- scripts/set-python.sh
+++ scripts/set-python.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 pip install $(awk '{print $1}' ../programs/pip.list)
+
+jupyter nbextension install --py --sys-prefix jupyter_ascending
+jupyter nbextension     enable jupyter_ascending --sys-prefix --py
+jupyter serverextension enable jupyter_ascending --sys-prefix --py

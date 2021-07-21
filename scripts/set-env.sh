f65101f1d850bb3a6c5ba9847d4a383d46dd79cb
Caio Dadauto <caiodadauto@gmail.com>
2021-07-21 11:57:43 -0300

    .

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -14,14 +14,12 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cd $HOME/.cargo/bin
-./cargo install $(awk '{print $1}' ../programs/cargo.list)
+./$HOME/.cargo/bin install $(awk '{print $1}' ../programs/cargo.list)
 
 # Starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 # Neovim
-cd ~
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
@@ -30,8 +28,7 @@ git clone https://github.com/wbthomason/packer.nvim\
 
 # Pyenv
 curl https://pyenv.run | bash
-cd $HOME/.pyenv/bin
-./pyenv install 3.8.9
-./pyenv global 3.8.9
-./pyenv virtualenv 3.8.9 ml
+./$HOME/.pyenv/bin/pyenv install 3.8.9
+./$HOME/.pyenv/bin/pyenv global 3.8.9
+./$HOME/.pyenv/bin/pyenv virtualenv 3.8.9 ml
 

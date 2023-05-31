a371dfa2767a42380b25b258dc270e74fd0ac5f4
Caio Dadauto <caiodadauto@gmail.com>
2023-05-31 16:56:55 -0300

    big change

--- scripts/set-deb-env.sh
+++ scripts/set-deb-env.sh
@@ -1,53 +1,36 @@
 #!/bin/bash
 
-#System
 printf "\n\n=====================SYSTEM=====================\n"
 mkdir -p ~/.config
 mkdir -p ~/.local/bin
 mkdir -p ~/.local/share/zsh
 sudo apt install $(awk '{print $1}' ../programs/apt.list)
 
-#Node
-printf "\n\n=====================Node=====================\n"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-#Node
 printf "\n\n=====================TMUX=====================\n"
 tmux
-tmux source-file ~/.config/tmux/tmux.conf 
+tmux source-file ~/.config/tmux/tmux.conf
 sudo cp ~/.config/systemd/user/tmux.service /etc/systemd/system/
 sudo systemctl enable tmux --now
 
-#NNN
 printf "\n\n=====================NNN=====================\n"
 cd /tmp
-wget https://github.com/jarun/nnn/archive/refs/tags/v4.4.tar.gz
+wget https://github.com/jarun/nnn/archive/refs/tags/v4.8.tar.gz
 tar xzf v4.4.tar.gz
-cd nnn-4.4
-make O_NERD=1
+cd nnn-4.8
+make O_NERD=1 O_GITSTATUS=1
 mv nnn ~/.local/bin
 cd ~
 
-#nsxiv
-printf "\n\n=====================NSXIV=====================\n"
+printf "\n\n=====================CHAFA=====================\n"
 cd /tmp
-git clone https://github.com/nsxiv/nsxiv.git
-cd nsxiv
-git checkout v30
+git clone https://github.com/hpjansson/chafa.git
+cd chafa
+git checkout 1.12.5
+./autogen.sh
 make
-mv nsxiv ~/.local/bin
+mv tools/chafa/chafa ~/.local/bin
 cd ~
 
-#latexindent
-printf "\n\n=====================LATEXINDENT=====================\n"
-cd /tmp
-git clone https://github.com/cmhughes/latexindent.pl
-cd latexindent.pl
-perl Makefile.PL
-sudo make install
-cd ~
-
-#dragon
 printf "\n\n=====================DRAGON=====================\n"
 cd /tmp
 git clone https://github.com/mwh/dragon.git
@@ -57,12 +40,10 @@ make
 mv dragon ~/.local/bin
 cd ~
 
-# FZF
 printf "\n\n=====================FZF=====================\n"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# Rust
 printf "\n\n=====================RUST=====================\n"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 ./$HOME/.cargo/bin/cargo install $(awk '{print $1}' $HOME/Dotfiles/programs/cargo.list)
@@ -73,21 +54,17 @@ git checkout 13.0.0
 cargo build --features 'pcre2'
 mv target/debug/rg ~/.local/bin
 
-
-# Starship
 printf "\n\n=====================STARSHIP=====================\n"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -b $HOME/.local/bin
 
-# Neovim
 printf "\n\n=====================NEOVIM=====================\n"
 cd ~
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim.appimage
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 git clone https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# Mamba
 printf "\n\n=====================MAMBAFORGE=====================\n"
 curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
 bash Mambaforge-$(uname)-$(uname -m).sh
@@ -95,3 +72,5 @@ rm Mambaforge-$(uname)-$(uname -m).sh
 cd ~/.mambaforge/bin/
 ./conda config --set auto_activate_base false
 cd ~
+
+done=0;while (( $done <= 10 ));do echo done=$done;done=$((done+1));done
